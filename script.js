9e3470252c7f1754e2eb72a019d1dc791ab94987
Mita-M-S <142704920+Mita-M-S@users.noreply.github.com>
2023-11-07 10:16:34 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add interactivity here, e.g., handling animations, form validation, etc.
+
+// Example: Animate the header on hover
+const header = document.querySelector("header");
+header.addEventListener("mouseenter", () => {
+  header.style.backgroundColor = "#555";
+});
+header.addEventListener("mouseleave", () => {
+  header.style.backgroundColor = "#333";
+});
